fe22ad888eb268f571beba79f39ae4f3b7e770c4
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-10-03 14:36:57 +0700

    Update remove some redundant code

--- DocumentStoreManagement/Program.cs
+++ DocumentStoreManagement/Program.cs
@@ -1,21 +1,12 @@
-using DocumentStoreManagement.Core.Interfaces;
 using DocumentStoreManagement.Infrastructure;
-using DocumentStoreManagement.Infrastructure.Repositories.Mongo;
-using DocumentStoreManagement.Services;
-using DocumentStoreManagement.Services.Interfaces;
-using Microsoft.Extensions.Options;
+using DocumentStoreManagement.Infrastructure.ServiceExtension;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDBDatabase"));
-builder.Services.AddSingleton<IMongoDbSettings>(sp =>
-    sp.GetRequiredService<IOptions<MongoDbSettings>>().Value);
-builder.Services.AddScoped<IMongoApplicationContext, MongoApplicationContext>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(MongoGenericRepository<>));
-builder.Services.AddScoped<IDocumentService, DocumentService>();
-//builder.Services.AddDIServices(builder.Configuration);
+builder.Services.AddDIServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
