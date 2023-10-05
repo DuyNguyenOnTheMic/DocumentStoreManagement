beada40b1c36256ac3e4ac8dc27d203978873275
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-10-05 17:57:15 +0700

    Add constants class

---
+++ DocumentStoreManagement/Helpers/Constants.cs
@@ -0,0 +1,22 @@
+﻿using DocumentStoreManagement.Core.Models.MongoDB;
+
+namespace DocumentStoreManagement.Helpers
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    /// <summary>
+    /// Constants class for reusable variables
+    /// </summary>
+    public class Constants
+    {
+        public static readonly int DocumentBookType = 1;
+        public static readonly int DocumentMagazineType = 2;
+        public static readonly int DocumentNewsPaperType = 3;
+
+        public static readonly Dictionary<int, string> DocumentTypes = new()
+        {
+            {DocumentBookType, nameof(Book) },
+            {DocumentMagazineType, nameof(Magazine) },
+            {DocumentNewsPaperType, nameof(Newspaper) }
+        };
+    }
+}
